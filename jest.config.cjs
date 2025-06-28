8d4e99f78503fc287ea4dc63c285afe39769150a
MovemDimon <movem9013@gmail.com>
2025-06-28 08:26:05 +0330

    Update and rename jest.config.js to jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,16 +1,12 @@
-/** @type {import('jest').Config} */
+// jest.config.cjs
 module.exports = {
-  // نام پکیج محیط تست
   testEnvironment: "jest-environment-miniflare",
   testEnvironmentOptions: {
-    // برای ماژول‌های ESM
     modules: true,
-    // دوام‌‌آبجکت‌ها
     durableObjects: {
       BOTNAMESPACE: "BotNamespace",
       WSNAMESPACE: "WSNamespace"
     },
-    // شبیه‌سازی متغیرهای env
     bindings: {
       PAYMENT_SERVERS: "https://example.com/api/transaction",
       WS_API_KEY: "test-key",
